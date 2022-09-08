2d6e70d682cf9b7993347d7019b35c554601d640
ben <benoit.frontzak@gmail.com>
2022-09-08 15:39:45 +0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MotorolaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
